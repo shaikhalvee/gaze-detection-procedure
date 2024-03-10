f8e7a2b489e734d51c58bb169de0903efc614e6e
Shaikh Islam <shaikhalvee@gmail.com>
2024-03-10 04:29:55 -0400

    files updated

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
+import cv2
+import numpy as np
+import dlib
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+def gaze_detect():
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 640)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+    while True:
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        faces = detector(gray)
+        for face in faces:
+            # print(face)
+            x, y = face.left(), face.top()
+            x1, y1 = face.right(), face.bottom()
+            w, h = face.width(), face.height()
+            # cv2.rectangle(frame, (x, y), (x1, y1), (0, 255, 0), 2)
+            landmarks = predictor(gray, face)
+            x = landmarks.part(36).x
+            y = landmarks.part(36).y
+            cv2.circle(frame, (x, y), 2, (0, 0, 255), 2)
+
+        cv2.imshow('frame', frame)
+
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    gaze_detect()
